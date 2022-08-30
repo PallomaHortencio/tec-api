7c071e1325ba94c61708bc92ee94e73f9b07847e
Palloma Hortencio <palloma.hortencio@gmail.com>
2022-08-30 11:58:11 -0300

    Fazendo os comandos de funções

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {ler} from "./src/aluno.js";
+import {ler, inserir, lerUm, atualizar} from "./src/aluno.js";
 
 const app = express();
 const porta = 3000;
@@ -26,12 +26,21 @@ app.get('/alunos', (req, res) => {
 
 /* Rota (endpoint) para exibir um unico aluno */
 app.get('/alunos/:id', (req, res) => {
-    res.send('Exibindo dados de UM aluno');
+   // res.send('Exibindo dados de UM aluno');
+
+   const id = req.params.id;
+   lerUm(id, res);
 })
 
 /* Post - rota para inserir alunos */
 app.post('/alunos', (req,res) => {
-    res.send('Inserindo alunos');
+   // res.send('Inserindo alunos');
+
+   // capturando os dados a partit do corpo da requisição
+   const novoAluno = req.body;
+
+   // executando a função inserir e passando os parametros novoAluno e res
+   inserir(novoAluno, res);
 })
 
 /* Put - atualizar TODOS os dados do aluno */
@@ -41,7 +50,14 @@ app.put('/alunos/:id', (req, res) => {
 
 /* Patch - atualizar alguns/todos os dados do aluno */
 app.patch('/alunos/:id', (req, res) => {
-    res.send('Atualizar alguns/todos os dados de um aluno');
+    // res.send('Atualizar alguns/todos os dados de um aluno');
+
+    // capturar id
+    const id = parseInt(req.params.id); // parseInt é uma camada de segurança a mais para o id
+    // dados aluno
+    const aluno = req.body;
+
+    atualizar(id, aluno, res); // a ordem tem que ser igual a da função
 })
 
 /* Delete - excluir aluno */
--- src/aluno.js
+++ src/aluno.js
@@ -22,4 +22,64 @@ conexao.query(sql, (erro, resultados) => {
 })
 }
 
-export {ler};
+
+// Inserindo alunos
+
+function inserir(aluno, res) {
+    // o trecho "SET, ?" estão vindo do MySql2 e a "?" recebe os dados atribuindo na ordem. Proteção contra Injection e Tratamento de strings vindos do modulo MySql2
+    // ? é um caracter coringa, pegara todos os parametros sem a necessidade de fazer o codigo igual ao php
+    const sql = "INSERT INTO alunos SET ?";
+
+    conexao.query(sql, aluno, (erro) => {
+        if(erro){
+            res.status(400).json(erro.code); // 400 - requisição inválida e informa o codigo de erro
+        } else {
+            res.status(201).json({"status": "aluno inserido"}); // 200 - criado e apresenta a mensagem aluno inserido
+            // res.status(200).end();
+        }
+    })
+}
+
+
+// função que exibe UM aluno
+function lerUm(id, res) {
+    const sql = "SELECT * FROM alunos WHERE id = ?";
+
+    conexao.query(sql, id, (erro, resultados) => {
+        // checando se existe conteudo
+        if(resultados.length === 0){
+            res.status(204).end();
+            return;
+        }
+
+        // if erro ou resultado
+        if(erro){
+             res.status(400).json(erro.code);
+        } else {
+            res.status(200).json(resultados[0]); // sem o [0] o codigo vem dentro da matriz, com ele manda apenas o objeto (codigo mais amigavel) 
+        }
+    });
+}
+
+
+// função atualizar
+//essa função vai receber um id, os dados do aluno e res
+function atualizar(id, aluno, res) {
+    const sql = "UPDATE alunos SET ? WHERE id = ?";
+
+    // a ordem do [aluno, id] importa muito, porque na const vai pegar primeiro os dados do aluno e depois seu id, precisa correponder ao sql acima
+    // para passar mais de um paramentro usa-se []
+    conexao.query(sql, [aluno, id], (erro, resultados) => {
+        if(erro) {
+            res.status(400).json(erro.code);
+        } else {
+          //  res.status(200).json({"status" : "Atualizado com sucesso"})
+
+          // spread operator (operador de "espalhamento" de objeto) deixa o codigo amigavel e tudo em uma matriz so
+          res.status(200).json({...aluno, id});
+        }
+    });
+}
+
+
+export {ler, inserir, lerUm, atualizar};
